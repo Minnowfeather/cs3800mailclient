a6242f99521fec7add2dba747f72b070b2fe2a1f
minnowfeather <matthewparedes2k3@gmail.com>
2024-04-15 22:36:01 -0700

    Test data changed a bit

--- mailbackend.py
+++ mailbackend.py
@@ -42,7 +42,7 @@ class mailbackend:
         self._server.sendmail(self._email, msg['To'], msg.as_string())
 
     def getInbox(self):
-        return [{"subject":"bingus", "sender": "mail1@gmail.com", "body":"bongus"},
+        return [{"subject":"test mail", "sender": "mail1@gmail.com", "body":"bongus"},
                 {"subject":"mailtwo", "sender": "mail2@gmail.com", "body":"this is the second email"},
                 {"subject":"this is the third mail", "sender": "mail3@gmail.com", "body":"epic mail time"}
 
